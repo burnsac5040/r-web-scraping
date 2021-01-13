dd2c19622a108699b283c1a09b899d5ab10104af
Lucas Burns <burnsac@me.com>
2021-01-13 12:21:01 -0600

    added investorhub

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 Web Scraping with R
 ===================
 
-[Hacker News](hackernews)
 -----------
+[Hacker News](hackernews) [WIP]
 
-Learning how to program in R as well. 
+Scraping the title of the post, external website, user, points/upvotes, time it was posted, and number of comments in an attempt to see if there are any keywords or external website that makes a post get more upvotes.
 
-This is a work in progress.
+-----------
+[Investor Hub](investorhub) [WIP]
+
+I wanted to test my knowledge of R as well as learn more by scraping stock data again.  Originally I scraped from [tiingo.com](tiingo.com) using Python, but this time I scraped from [investorshub.com](investorshub.advfn.com).  The headlines on this website could be used more so for a script to help one invest.  I decided to upload this anyway.
---
+++ investorhub/r-invhub.R
@@ -0,0 +1,58 @@
+library(dplyr)
+library(stringr)
+library(flipTime)
+library(httr)
+library(rvest)
+library(XML)
+library(RCurl)
+
+#-----------
+# One Page #
+#-----------
+base_url <- "https://investorshub.advfn.com/boards/recentnews.aspx?page=11"
+
+parsed <- htmlParse(GET(base_url), encoding="UTF-8")
+wtable <- readHTMLTable(parsed)
+wtable <- wtable[[1]]
+names(wtable) <- c('symb', 'quote', 'change', 'pcnt', 'headline', 'time')
+wtable$change <- as.numeric(wtable$change)
+wtable$pcnt <- wtable$pcnt %>% gsub("%", "", .) %>% as.numeric()
+wtable$time <- AsDateTime(wtable$time)
+
+
+# ---------------
+# Mutiple Pages #
+# ---------------
+base_url <- "http://investorshub.advfn.com/boards/recentnews.aspx?page="
+
+hub_df <- data.frame()
+i = 1
+
+while (i < 30){
+  parsed <- htmlParse(GET(paste0(base_url, i), encoding="UTF-8"))
+  wtable <- readHTMLTable(parsed)
+  wtable <- wtable[[1]]
+
+  namecols <- c('symb', 'quote', 'change', 'pcnt', 'headline', 'time')
+  names(wtable) <- namecols
+
+  wtable$quote <- wtable$quote %>% gsub(",", "", .) %>% as.numeric()
+  wtable$change <- as.numeric(wtable$change)
+  wtable$pcnt <- wtable$pcnt %>% gsub("%", "", .) %>% as.numeric()
+  wtable$time <- AsDateTime(wtable$time)
+
+  hub_df <- rbind(hub_df, wtable)
+  colnames(hub_df) <- namecols
+
+  i = i + 1
+  Sys.sleep(3)
+}
+
+hub_df <- hub_df[complete.cases(hub_df), ] # removing NA's
+colSums(is.na(hub_df)) # check to make sure there are no NA's
+
+write.csv(hub_df, file='hub_df.csv', row.names=FALSE)
+write.table(hub_df, file='hub_df.tsv', sep="\t", row.names=FALSE,
+			col.names=FALSE, quote=FALSE)
+
+
