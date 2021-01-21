0ed1415b847246793ec41ffeec5a3b798d98c4d5
Lucas Burns <burnsac@me.com>
2021-01-21 16:13:58 -0600

    deleted some, new 50 scrape

--- finviz/finviz-nlp.R
+++ finviz/finviz-nlp.R
@@ -28,6 +28,7 @@ sp500_tickers <- sp500[, c("Tickers", "Company", "GICS.Sector")]
 samp_sp500 <- slice_sample(sp500_tickers, n=250)
 samp_sp500 <- samp_sp500[grep("\\.", samp_sp500, invert = TRUE)]
 # samp_sp500 <- samp_sp500[!grepl("\\.", samp_sp500$Tickers), ]
+
 samp_tick <- samp_sp500$Tickers
 
 base_url <- "https://finviz.com/quote.ashx?t="
@@ -86,8 +87,8 @@ pnews_df <- do.call(rbind, parsed_dfs)
 
 pnews_df <- as.data.frame(fread("data/50pnews_df-dirty.tsv", quote = "", header = TRUE))
 sp500_df <- as.data.frame(fread("data/50sector_df.tsv", quote = ""))
+
 colnames(pnews_df) <- c("ticker", "date", "time", "headline")
-pnews_df
 # pnews_df <- pnews_df[,-1]
 # pnews_df <- pnews_df[, !(names(pnews_df) %in% c("pdatetime"))]
 
@@ -96,37 +97,37 @@ pnews_df$datetime <- AsDateTime(pnews_df$datetime_, us.format = TRUE)
 pnews_df$datetime_ <- NULL
 
 pnews_df$headline %<>% tolower()
-pnews_df$headline <- gsub("(yahoo finance|barrons.com|motley fool|zacks|reuters|insider monkey|business wire|marketwatch|smarteranalyst|benzinga|globenewswire|techcrunch|quartz|bloomberg|investopedia|newswire|tipranks)", "", pnews_df$headline)
+
+# pnews <- copy(pnews_df)
+
+pnews_df$headline <- gsub("[[:blank:]][[:alpha:]]*\\.com$", "", pnews_df$headline) # Deletes website.com
+# pnews_df$headline <- gsub("\\s*\\w*$", "", pnews_df$headline) # Deletes last word
+
+pnews_df$headline <- gsub("(investorplace|yahoo finance|motley fool|zacks|reuters|insider monkey|business wire|marketwatch|smarteranalyst|benzinga|globenewswire|techcrunch|quartz|bloomberg|investopedia|newswire|tipranks|gurufocus.com)", "", pnews_df$headline)
 pnews_df$headline <- gsub("([[:punct:]]|[[:digit:]])", "", pnews_df$headline)
+pnews_df$headline <- gsub("\\bpr\\b", "", pnews_df$headline)
 pnews_df$headline <- str_squish(pnews_df$headline)
+
 
 ####################################################
 ###################### NLP #########################
 ####################################################
 news_df <- copy(pnews_df)
 
+# Separate dataframes of before the January 6th riot and after
 befjan <- news_df[news_df$datetime < as.POSIXct("2021-01-06"), ]
 aftjan <- news_df[news_df$datetime >= as.POSIXct("2021-01-06"), ]
 
-riot <- aftjan[aftjan$headline %like% "riot", ]
-capitol <- aftjan[aftjan$headline %like% "capitol", ]
-qanon <- aftjan[aftjan$headline %like% "qanon", ]
-war <- aftjan[aftjan$headline %like% "war", ]
-
-library(NLP)
-library(tm)
-library(wordcloud)
-library(RColorBrewer)
-library(sentimentr)
-library(tidytext)
-
-test <- slice_sample(news_df, n=20)
-
+# Search for keywords
+# NOTE: Alternative way to search for a keyword, however the above picks up more
+# riot <- aftjan[aftjan$headline %like% "riot", ]
 riot <- news_df %>% filter(str_detect(news_df$headline, "riot"))
-riot
+capitol <- news_df %>% filter(str_detect(news_df$headline, "capitol"))
+qanon <- news_df %>% filter(str_detect(news_df$headline, "qanon"))
+war <- news_df %>% filter(str_detect(news_df$headline, "war"))
 
-write.table(pnews_df, file="data/pnews_df.tsv", quote = FALSE, sep="\t", row.names = FALSE)
-write.csv(pnews_df, file = "data/pnews_df.csv")
+# write.table(pnews_df, file="data/pnews_df.tsv", quote = FALSE, sep="\t", row.names = FALSE)
+# write.csv(pnews_df, file = "data/pnews_df.csv")
 
 #####################################################
 #################### TESTING NLP ####################
@@ -135,15 +136,44 @@ write.csv(pnews_df, file = "data/pnews_df.csv")
 library(Rstem)
 library(sentiment)
 
-polarity_df <- classify_polarity(news_df$headline, algorithm = "bayes")
+library(NLP)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(sentimentr)
+library(tidytext)
+
+# Classify polarity and emotion
+polarity_df <- classify_polarity(pnews_df$headline, algorithm = "bayes")
+emotion_df <- classify_emotion(pnews_df$headline, algorithm = "bayes", prior = 1.0)
+
+# Read back in the dataframes
+pnews_df <- as.data.frame(fread("data/50pnews_df.tsv", quote = "", header = TRUE))
 polarity_df <- as.data.frame(fread("data/50polarity_df.tsv", quote = "", header = TRUE))
+emotion_df <- as.data.frame(fread("data/50emotion_df.tsv", quote = "", header = TRUE))
 
-polarity_df <- cbind(pnews_df, polarity_df)
-pos <- polarity_df[polarity_df$BEST_FIT == 'positive', ]
-neg <- polarity_df[polarity_df$BEST_FIT == 'negative', ]
+names(polarity_df)[names(polarity_df) == "BEST_FIT"] <- "pol_best"
+names(emotion_df)[names(emotion_df) == "BEST_FIT"] <- "emo_best"
 
-for (i in c(1:nrow(pos$headline))){
-  pos$headline[i, 1] <- wordStem(String(pos$headline[i, 1]))
+# Create a larger combined dataframe
+emopol_df <- cbind(pnews_df, polarity_df, emotion_df)
+emopol_df
+
+pos <- emopol_df[emopol_df$pol_best == 'positive', ]
+neg <- emopol_df[emopol_df$pol_best == 'negative', ]
+
+xxx <- slice_sample(pos, n=30)
+st <- xxx[['headline']]
+
+get_wordlist <- function(list_){
+  wordlist <- c()
+  for (line_ in list_){
+    words_ <- words(line_)
+    for (word_ in words_){
+      wordlist <- append(wordlist, word_)
+    }
+  }
+  return(unique(wordlist))
 }
 
 stem <- function(list){
@@ -154,10 +184,16 @@ stem <- function(list){
 }
 
 
+ww <- get_wordlist(st)
+ww
 
+for (i in c(1:length(ww))){
 
-for (i in c(1:dim(pos)[[1]])){
-  print(i)
 }
 
-r <- unique
+
+
+
+
+positive <- stem(ww)
+positive
